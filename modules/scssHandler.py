61bcc208cf574d5218e195507bc00916661a7142
serii burduja <seriiburduja@gmail.com>
2024-10-15 10:19:21 +0300

    feat: remove empty lines

--- modules/scssHandler.py
+++ modules/scssHandler.py
@@ -3,18 +3,18 @@ from libs.buffer import addToClipBoardFile
 from modules.convertToRem import convertToRem
 from modules.convertVariables import convertVariables
 from modules.deleteLines import deleteLines
-from modules.printTemp import printTemp
 from modules.removeComment import removeComment
+from modules.removeEmptyLines import removeEmptyLines
 from modules.sortLines import sortLines
 
 
 def scssHandler(file_path, variables_path):
     removeComment(file_path)
-    # printTemp(file_path)
     convertToRem(file_path)
     deleteLines(file_path)
     convertVariables(file_path, variables_path)
     sortLines(file_path)
+    removeEmptyLines(file_path)
     with open(file_path, 'r') as file:
         addToClipBoardFile(file_path)
         file_content = file.read()
