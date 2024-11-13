af5b8b8f36a0e32b595f846d4bfc3c136f530200
serii burduja <seriiburduja@gmail.com>
2024-11-13 10:55:28 +0200

    feat: font-kerning delete

--- modules/deleteLines.py
+++ modules/deleteLines.py
@@ -2,7 +2,7 @@ def deleteLines(file_path):
     with open(file_path, 'r') as file:
         data = file.readlines()
     for i in range(len(data)):
-        to_delete = ['font-style: normal', 'line-height: normal', 'font-feature-settings', 'text-overflow', 'overflow']
+        to_delete = ['font-style: normal', 'line-height: normal', 'font-feature-settings', 'text-overflow', 'overflow', 'font-kerning']
         if any(prop in data[i] for prop in to_delete):
             data[i] = ''
     with open(file_path, 'w') as file:
