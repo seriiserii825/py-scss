8ebb4b53830ef5b622a06ba4e76c7bee1b3ce7cc
serii burduja <seriiburduja@gmail.com>
2025-06-07 15:00:16 +0300

    feat: install pyfzf

--- libs/select.py
+++ libs/select.py
@@ -1,4 +1,6 @@
 from simple_term_menu import TerminalMenu
+
+
 def selectOne(options):
     terminal_menu = TerminalMenu(options)
     # menu_entry_index = terminal_menu.show()
@@ -7,6 +9,7 @@ def selectOne(options):
 
 # selectOne(getThemes())
 
+
 def selectMultiple(options):
     terminal_menu = TerminalMenu(options,
                                  multi_select=True,
@@ -14,8 +17,4 @@ def selectMultiple(options):
                                  show_search_hint=True,
                                  preview_command="bat --color=always {}", preview_size=0.75
                                  )
-    menu_entry_indices = terminal_menu.show()
-    # print(menu_entry_indices)
-    # print(terminal_menu.chosen_menu_entries)
     return terminal_menu.chosen_menu_entries
-
