7c24d177a36098bc2de4431f78128e2fcf3c2703
serii burduja <seriiburduja@gmail.com>
2025-06-17 09:34:49 +0300

    feat: convert to rem

---
+++ modules/has_convert_to_rem.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+
+def has_convert_to_rem():
+    ROOT_DIR = Path(__file__).parent
+    config_file = f"{ROOT_DIR}/../utils/config.txt"
+
+    if not Path(config_file).is_file():
+        raise FileNotFoundError(f"Configuration file not found: {config_file}")
+    with open(config_file, 'r') as file:
+        lines = file.readlines()
+        for line in lines:
+            if 'CONVERT_TO_REM' in line:
+                result = line.split('=')[1].strip()
+                return result
+            raise ValueError("CONVERT_TO_REM not found in the configuration file.")
--- modules/scssHandler.py
+++ modules/scssHandler.py
@@ -3,6 +3,7 @@ from libs.buffer import addToClipBoardFile
 from modules.convertToRem import convertToRem
 from modules.convertVariables import convertVariables
 from modules.deleteLines import deleteLines
+from modules.has_convert_to_rem import has_convert_to_rem
 from modules.removeComment import removeComment
 from modules.removeEmptyLines import removeEmptyLines
 from modules.sortLines import sortLines
@@ -10,7 +11,17 @@ from modules.sortLines import sortLines
 
 def scssHandler(file_path, variables_path):
     removeComment(file_path)
-    convertToRem(file_path)
+    try:
+        convert_to_rem = has_convert_to_rem()
+        print(f"convert_to_rem: {convert_to_rem}")
+    except FileNotFoundError as e:
+        print(f"Error: {e}")
+        return
+    except ValueError as e:
+        print(f"Error: {e}")
+        return
+    if convert_to_rem == '1':
+        convertToRem(file_path)
     deleteLines(file_path)
     convertVariables(file_path, variables_path)
     sortLines(file_path)
