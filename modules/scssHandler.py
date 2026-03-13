800ac8949be1421d2d0ce9fdf8cf838daf6b76cb
serii burduja <seriiburduja@gmail.com>
2026-03-13 17:27:54 +0200

    feat: convert to rem

--- modules/scssHandler.py
+++ modules/scssHandler.py
@@ -11,7 +11,21 @@ from modules.sortLines import sortLines
 
 
 def scssHandler(file_path, variables_path):
+    agree_to_rem = input("Do you want to convert px to rem? (y/n): ")
+    if agree_to_rem.lower() == "y":
+        convert_to_rem_func(file_path)
     removeComment(file_path)
+    deleteLines(file_path)
+    convertVariables(file_path, variables_path)
+    sortLines(file_path)
+    removeEmptyLines(file_path)
+    with open(file_path, "r") as file:
+        addToClipBoardFile(file_path)
+        file_content = file.read()
+        subprocess.Popen(["notify-send", file_content])
+
+
+def convert_to_rem_func(file_path: str):
     try:
         convert_to_rem = has_convert_to_rem()
         print(f"convert_to_rem: {convert_to_rem}")
@@ -23,11 +37,3 @@ def scssHandler(file_path, variables_path):
         return
     if convert_to_rem == "1":
         convertToRem(file_path)
-    deleteLines(file_path)
-    convertVariables(file_path, variables_path)
-    sortLines(file_path)
-    removeEmptyLines(file_path)
-    with open(file_path, "r") as file:
-        addToClipBoardFile(file_path)
-        file_content = file.read()
-        subprocess.Popen(["notify-send", file_content])
